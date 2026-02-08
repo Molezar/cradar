209b1ac0f3538d21979119e3848253ffa2962046
Molezar <blackviewbw6000@gmail.com>
2026-02-08 13:48:53 +0200

    cluster bug fix

--- server.py
+++ server.py
@@ -1,16 +1,16 @@
-from flask import Flask, jsonify, send_from_directory
+from flask import Flask, jsonify, send_file
 from onchain import build_cluster
+import os
 
-app = Flask(__name__, static_folder=".")
+app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return send_from_directory(".", "index.html")
+    return send_file("index.html")
 
 @app.route("/cluster")
 def cluster():
-    data = build_cluster()
-    return jsonify(data)
+    return jsonify(build_cluster())
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
